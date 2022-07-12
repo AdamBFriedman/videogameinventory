778ff6200ea9280ab6e752c3b8dbabdb5820de32
adambfriedman <adam.friedman5@gmail.com>
2022-07-11 22:47:10 -0500

    Add custom logger middleware

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const { logger } = require('./middleware/logEvents')
 const PORT = process.env.PORT || 8000;
+
+// Custom logger
+app.use(logger);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
