62e67b0020311b10af2f5199795ff633ddb960cc
adambfriedman <adam.friedman5@gmail.com>
2022-07-11 23:00:20 -0500

    Add error routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const { logger } = require('./middleware/logEvents')
 const PORT = process.env.PORT || 8000;
 
@@ -8,5 +9,17 @@ app.use(logger);
 
 // Routes
 app.use("/", require("./routes/root"));
+
+// Error routes
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+      res.sendFile(path.join(__dirname, "pages", "error.html"));
+    } else if (req.accepts("json")) {
+      res.json({ error: "404: Not Found" });
+    } else {
+      res.type("txt").send("404: Not Found");
+    }
+  });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
