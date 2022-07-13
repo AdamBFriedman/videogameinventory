57290afeecae645935c210e8ca8f626664cd8b22
adambfriedman <adam.friedman5@gmail.com>
2022-07-12 19:09:32 -0500

    FE successfully consuming local api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const url = "http://localhost:8000/games";
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json;charset=UTF-8",
+  },
+};
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      setGames(data);
+    });
+  }, [])
+  
   return (
     <div className="App">
       <h1>Video Games</h1>
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
