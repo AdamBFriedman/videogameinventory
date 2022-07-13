0b73d0e39572c39d7e62a52ffd2d4ed3ce833140
adambfriedman <adam.friedman5@gmail.com>
2022-07-12 19:02:47 -0500

    Update cors option

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,6 +4,7 @@ const whitelist = [
     // When you click the "Go Live" button in VS Code
     "http://127.0.0.1:5500",
     // Local environment
+    "http://localhost:3000",
     "http://localhost:8000",
   ];
   
