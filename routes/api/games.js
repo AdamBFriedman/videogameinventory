363751c5d3497077f840a901f9a9ef03c5466962
adambfriedman <adam.friedman5@gmail.com>
2022-07-13 22:01:02 -0500

    Add json web token verification middleware

--- routes/api/games.js
+++ routes/api/games.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getAllGames, addGame, updateGame, deleteGame, getGame } = require('../../controllers/gamesController');
 const router = express.Router();
+const verifyJWT = require('../../middleware/verifyJWT')
 
 router.route('/')
-.get(getAllGames)
+.get(verifyJWT, getAllGames)
 .post(addGame)
 .put(updateGame)
 .delete(deleteGame)
