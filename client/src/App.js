90e7cd4e66d8cb9c833d031ca13bfcd4944ac359
adambfriedman <adam.friedman5@gmail.com>
2022-07-20 16:57:58 -0500

    Add theme to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,33 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import {
+  createTheme,
+  Theme,
+  ThemeProvider,
+} from '@mui/material/styles';
 import './App.css';
 import Home from './components/Home';
 import Games from './components/Games';
 import { Logout } from './components/Logout';
+import { CssBaseline } from '@mui/material';
+
+const theme = createTheme({});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Logout />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/games" element={<Games />} />
-        </Routes>
-      </>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <>
+          <Logout />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/games" element={<Games />} />
+          </Routes>
+        </>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,23 @@ import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import LoginForm from './LoginForm';
 import { useNavigate } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  background: {
+    background: 'red',
+    height: '100vh',
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
   const navigate = useNavigate();
   useEffect(() => {
     if (localStorage.getItem('JWT')) navigate('/games');
   }, []);
   return (
-    <Box>
+    <Box className={classes.background}>
       <h1>Login</h1>
       <LoginForm />
     </Box>
