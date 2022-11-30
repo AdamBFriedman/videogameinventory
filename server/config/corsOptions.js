d03e01ecc425f0732fa3b4b378fb154d0f788d73
adambfriedman <adam.friedman5@gmail.com>
2022-11-29 20:10:07 -0600

    Update server for production.

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,23 +1,24 @@
 const whitelist = [
-    "https://www.yoursite.com",
-    // TODO: Remove the below links in production
-    // When you click the "Go Live" button in VS Code
-    "http://127.0.0.1:5500",
-    // Local environment
-    "http://localhost:3000",
-    "http://localhost:8000",
-  ];
-  
-  const corsOptions = {
-    origin: (origin, callback) => {
-      // Check if domain is in our whitelist
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  };
-  
-  module.exports = corsOptions;
+  'https://www.yoursite.com',
+  // TODO: Remove the below links in production
+  // When you click the "Go Live" button in VS Code
+  'http://127.0.0.1:5500',
+  // Local environment
+  'http://localhost:3000',
+  'http://localhost:8000',
+  'https://videogameinventory.herokuapp.com/',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Check if domain is in our whitelist
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
